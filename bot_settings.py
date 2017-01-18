b7cdd5c89564dc54254fc716762ee20ef87fc44c
rsamuelson <beccasamuelson@gmail.com>
2017-01-17 23:05:28 -0800

    key clean-up

--- bot_settings.py
+++ bot_settings.py
@@ -1,5 +1,5 @@
-SLACK_TOKEN = 'xoxp-2464649799-5121668681-96835184753-347309e9519258d91d9cae78c99cbdf2'
-channel_id = 'C2NMCT7HN'
+SLACK_TOKEN = '<YOUR SLACK TOKEN HERE>'
+channel_id = '<YOUR CHANNEL ID HERE>'
 no_game_message = 'There are no games tonight. Go home, get the family, and watch SonicsGate!'
 game_message = 'Be careful! There is a homegame in Seattle tonight. And no, it aint the Sonics.'
 
